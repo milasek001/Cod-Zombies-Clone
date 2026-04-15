6406b1ace83118cb66999f8a955d6baaa952c306
milasek001 <holubar2004@seznam.cz>
2026-04-15 18:25:21 +0200

    Add files via upload

---
+++ Scripts/Coin.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class Coin : Area2D
+{
+	private void OnBodyEntered(Node2D body)
+{
+	GD.Print("+1 coin");
+	QueueFree();
+}
+}
+ 
---
+++ Scripts/Player.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 100.0f;
+	public const float JumpVelocity = -300.0f;
+	
+	private AnimatedSprite2D _animatedSprite2d => GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("move_left", "move_right", "ui_up", "ui_down");
+		
+		if (direction.X > 0) 
+		{
+			_animatedSprite2d.FlipH = false;
+		} else if(direction.X < 0) 
+		{
+			_animatedSprite2d.FlipH = true;
+		}
+		if (IsOnFloor())
+		{
+			if (direction.X == 0) 
+		{
+			_animatedSprite2d.Play("Idle");
+		} else 
+			_animatedSprite2d.Play("Run");
+		}
+		else 
+		{
+			_animatedSprite2d.Play("Jump");
+		}
+		
+		
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
